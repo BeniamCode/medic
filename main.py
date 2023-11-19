4058ece926263b2a6c8b6e7cd92eb23ef2aafd83
beniam <dbeniam@gmail.com>
2023-11-19 19:27:25 +0200

    add index page and call page

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from typing import Union
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.requests import Request
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def read_root():
-    return {"The fire is on!"}
-
-@app.get("/items/{item_id}")
-
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
